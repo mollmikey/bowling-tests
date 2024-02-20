9ca8047b0a1f141dfaf47aa32cdaccd7e35355d8
Michael <104609759+mollmikey@users.noreply.github.com>
2024-02-21 12:14:42 +1300

    compelted matrix function

--- matrix.js
+++ matrix.js
@@ -1,4 +1,17 @@
 /* eslint-disable no-unused-vars */
-export function getMatrix(n) {}
+export function getMatrix(n) {
+  let arr = []
+  for (let i = 0; i < n; i++) {
+    let temp = []
+    for (let i = 0; i < n; i++) {
+      temp.push(0)
+    }
+    arr.push(temp)
+  }
+  return arr
+}
 
-export function updateMatrix(matrix, coords, value) {}
+export function updateMatrix(matrix, coords, value) {
+  matrix[coords[0]][coords[1]] = value
+  return matrix
+}
