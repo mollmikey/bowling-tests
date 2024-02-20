8c97bf3616d613e964ae476a60a4343b8a312d9a
Michael <104609759+mollmikey@users.noreply.github.com>
2024-02-21 11:30:46 +1300

    completed find function

--- find.js
+++ find.js
@@ -1,2 +1,13 @@
 /* eslint-disable no-unused-vars */
-export default function find(arr, searchDetails) {}
+export default function find(arr, searchDetails) {
+  return arr.find((item) =>
+    Object.values(item).includes(Object.values(searchDetails)[0])
+  )
+}
+
+// Other method below :)
+
+// //export default function find(arr, searchDetails) {
+//   console.log(searchDetails)
+//   return arr.find((item) => item.address === 'Wayne Manor')
+// }
