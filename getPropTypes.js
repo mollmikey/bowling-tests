30603a20eea32faf5c37228a425b14b63dc9c153
Sukh <47804169+sukhjeetChauhan@users.noreply.github.com>
2024-02-21 11:41:10 +1300

    completed getPropType

--- getPropTypes.js
+++ getPropTypes.js
@@ -1,2 +1,4 @@
 /* eslint-disable no-unused-vars */
-export default function getPropTypes(obj) {}
+export default function getPropTypes(obj) {
+  return Object.values(obj).map((item) => typeof item)
+}
