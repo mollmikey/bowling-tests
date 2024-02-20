9aaa6b4e15fe47aabfccf6711b3af61c0af021f8
Sukh <47804169+sukhjeetChauhan@users.noreply.github.com>
2024-02-21 12:54:52 +1300

    completed where function

--- where.js
+++ where.js
@@ -1,2 +1,15 @@
 /* eslint-disable no-unused-vars */
-export default function where(arr, searchDetails) {}
+export default function where(arr, searchDetails) {
+  if (Object.values(searchDetails).length === 1) {
+    return arr.filter((item) =>
+      Object.values(item).includes(Object.values(searchDetails)[0])
+    )
+  }
+  if (Object.values(searchDetails).length === 2) {
+    return arr.filter(
+      (item) =>
+        Object.values(item).includes(Object.values(searchDetails)[0]) &&
+        Object.values(item).includes(Object.values(searchDetails)[1])
+    )
+  }
+}
