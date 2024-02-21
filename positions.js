c2be801e1c17b401a93abf4777fe85c9c4242cf1
Sukh <47804169+sukhjeetChauhan@users.noreply.github.com>
2024-02-21 13:08:42 +1300

    compelted positions functions, completed exercise

--- positions.js
+++ positions.js
@@ -1,4 +1,9 @@
 /* eslint-disable no-unused-vars */
-export function getFirst(arr) {}
+export function getFirst(arr) {
+  return arr[0]
+}
 
-export function getLast(arr) {}
+export function getLast(arr) {
+  console.log(arr.at(-1))
+  return arr.at(-1)
+}
